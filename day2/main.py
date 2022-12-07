288834bf1d29601338bc015903f8d1f2a04580b7
tmerge <tmerge@uos.de>
2022-12-07 21:49:37 +0100

    add solution for part two of day two

--- day2/main.py
+++ day2/main.py
@@ -1,7 +1,9 @@
+from enum import Enum
 ## Player 1
 # A = Rock (1)
 # B = Paper (2)
 # C = Scissor (3)
+
 ## Player 2
 # X = Rock (1)
 # Y = Paper (2)
@@ -10,9 +12,17 @@
 # Win = 6
 # Draw = 3
 # Loose = 0
- 
-score = 0
-with open('input.txt', 'r') as gameset:
+
+class GAMESHAPES(Enum):
+    A = 1
+    B = 2
+    C = 3
+    X = 1
+    Y = 2
+    Z = 3
+
+def playNormal(gameset):
+    score = 0
     for game in gameset:
         game = game.split()
         
@@ -42,6 +52,48 @@ with open('input.txt', 'r') as gameset:
                 score += 2
             elif game[1] == 'Z':
                 score += 3
-        
+    return score
+    
+def playGuided(gameset):
+    score = 0
+    for game in gameset:
+        game = game.split()
+
+        # check for case you should loose
+        if game[1] == 'X':
+            if game[0] == 'A':
+                score += 3
+            elif game[0] == 'B':
+                score += 1
+            elif game[0] == 'C':
+                score += 2
+
+        # check for case you should get a draw
+        elif game[1] == 'Y':
+            score += 3
+            if game[0] == 'A':
+                score += 1
+            elif game[0] == 'B':
+                score += 2
+            elif game[0] == 'C':
+                score += 3
+
+        # check the case you should win
+        elif game[1] == 'Z':
+            score += 6
+            if game[0] == 'A':
+                score += 2
+            elif game[0] == 'B':
+                score += 3
+            elif game[0] == 'C':
+                score += 1 
+    return score
+
+with open('input.txt', 'r') as gameset:
+    #nscore = playNormal(gameset)    
+    gscore = playGuided(gameset)
     # print final score based on recommandations
-    print(f'Final score: {score}')
+    #print(f'Final nscore: {nscore}')
+
+    # print finaö score based on guide 
+    print(f'Final gscore: {gscore}')
